49cbfbce2a796f804a77b4596854093f93f44b27
La Fountain <github email address>
2018-11-27 17:15:30 -0500

    Group app project

---
+++ db/seeds.rb
@@ -0,0 +1,14 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+
+Bus.create(name: "37")
+Bus.create(name: "M100")
+Bus.create(name: "M10")
+Person.create(name: "Terrance")
+Person.create(name: "Jessie")
